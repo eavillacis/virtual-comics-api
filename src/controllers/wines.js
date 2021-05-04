ca8fc43ffac152003de09759f32e3f8d0a243804
Esteban Villacis <esteban.villacis@thoughtworks.com>
2021-05-03 22:49:30 -0500

    feat(wines): wines endpoint test

--- src/controllers/wines.js
+++ src/controllers/wines.js
@@ -11,19 +11,21 @@ exports.getWines = [
     const segment = AWSXRay.getSegment();
     segment.addAnnotation("segment", "get wines data");
 
-    try {
-      CSVToJSON()
-        .fromFile("https://wines-assets.s3-us-west-2.amazonaws.com/wines.csv")
-        .then((wines) => {
-          console.log('WINES ---> ', wines)                 
-          res.json(JSON.stringify(wines));
-        })
-        .catch((err) => {
-          // log error if any
-          console.log(err);
-        });
-    } catch (e) {
-      next(e);
-    }
+    res.json(["Status OK WINES"]);
+
+    // try {
+    //   CSVToJSON()
+    //     .fromFile("https://wines-assets.s3-us-west-2.amazonaws.com/wines.csv")
+    //     .then((wines) => {
+    //       console.log('WINES ---> ', wines)   
+    //       res.json(["Status OK WINES"]);       
+    //     })
+    //     .catch((err) => {
+    //       // log error if any
+    //       console.log(err);
+    //     });
+    // } catch (e) {
+    //   next(e);
+    // }
   },
 ];
