c431af34625990c6e4be466c37f18f91e9324683
Esteban Villacis <esteban.villacis@thoughtworks.com>
2021-05-04 00:01:42 -0500

    feat(wines): wines endpoint test

--- src/controllers/wines.js
+++ src/controllers/wines.js
@@ -1,33 +1,23 @@
 const AWSXRay = require("aws-xray-sdk");
 const CSVToJSON = require("csvtojson");
 
+const getWinesData = async () => {
+  try {
+    const csvFilePath =
+      "https://wines-assets.s3-us-west-2.amazonaws.com/wines.csv";
+    wines = await CSVToJSON().fromFile(csvFilePath);
+    return wines
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 /**
  * Comics List.
  *
  * @returns {Object}
  */
-exports.getWines = [
-  function (req, res, next) {
-    const segment = AWSXRay.getSegment();
-    segment.addAnnotation("segment", "get wines data");
-
-    let wines;
-    const getWinesData = async () => {
-      try {
-        const csvFilePath =
-          "https://wines-assets.s3-us-west-2.amazonaws.com/wines.csv";
-        wines = await CSVToJSON().fromFile(csvFilePath);
-        return wines
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    const result = await getWinesData(items);
-
-    console.log("wines ---> ", wines);
-    console.log("result ---> ", result);
-
-    res.json(["Status OK WINES"]);
-  },
-];
+exports.getWines = async (req, res) => {
+  const result = await getWinesData()
+  res.json(result)
+}
